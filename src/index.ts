78d3eb001fa906a5df97a75c0b8b03d949744130
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-01-26 16:53:59 -0500

    Set status to "Hello World"

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,16 @@
-import { showHUD, Clipboard } from "@raycast/api";
+import { getPreferenceValues, showHUD, Clipboard } from "@raycast/api";
+import { LogLevel, WebClient } from "@slack/web-api";
 
 export default async function main() {
+  const { slackToken } = getPreferenceValues<Preferences>();
+  const slack = new WebClient(slackToken, {
+    logLevel: LogLevel.DEBUG,
+  });
+  slack.users.profile.set({
+    profile: {
+      status_text: 'hello world'
+    }
+  });
   const now = new Date();
   await Clipboard.copy(now.toLocaleDateString());
   await showHUD("Copied date to clipboard");

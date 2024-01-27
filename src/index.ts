20c4b94de5a334f99e05fe3abd770dd2d5a0d14b
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-01-26 19:08:34 -0500

    Wire up argument names

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,31 @@
-import { getPreferenceValues, showHUD } from "@raycast/api";
+/**
+ * Set Slack Status and Emoji
+ *
+ * See https://api.slack.com/methods/users.profile.set
+ */
+import { getPreferenceValues, LaunchProps, showHUD } from "@raycast/api";
 import { LogLevel, WebClient } from "@slack/web-api";
 
-export default async function main() {
+type StatusArgs = {
+  message: string;
+  emoji: string;
+};
+
+export default async function main(props: LaunchProps<{ arguments: StatusArgs }>) {
+  const {
+    arguments: {
+      message,
+      emoji
+    }
+  } = props;
   const { slackToken } = getPreferenceValues<Preferences>();
   const slack = new WebClient(slackToken, {
     logLevel: LogLevel.DEBUG,
   });
   slack.users.profile.set({
     profile: {
-      status_text: 'hello world'
+      status_text: message,
+      status_emoji: `${emoji}`
     }
   });
   await showHUD("Done");

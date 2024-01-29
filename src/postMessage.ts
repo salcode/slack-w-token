483a35ab07261d2a169aef44b2d819c7df61ba23
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2024-01-28 20:54:11 -0500

    Add error catching to postMessage

--- src/postMessage.ts
+++ src/postMessage.ts
@@ -4,6 +4,7 @@
  * See https://api.slack.com/methods/chat.postMessage
  */
 import { LaunchProps, showHUD } from "@raycast/api";
+import { ErrorCode } from "@slack/web-api";
 import getSlackWebClient from './utils/getSlackWebClient';
 
 type StatusArgs = {
@@ -20,10 +21,26 @@ export default async function main(props: LaunchProps<{ arguments: StatusArgs }>
   } = props;
 
   const slack = getSlackWebClient();
-  slack.chat.postMessage({
-    as_user: true,
-    text: message,
-    channel: channel
-  });
-  await showHUD("Done");
+  try {
+    const result = await slack.chat.postMessage({
+      as_user: true,
+      text: message,
+      channel: channel
+    });
+    await showHUD(`Successfully sent message (${result.ts}) in ${channel}`);
+  } catch(error: any) {
+    if (ErrorCode.PlatformError === error?.code) {
+      console.log('slack postMessage PlatformError');
+      console.log(error?.data);
+      await showHUD(`Failed to post message: ${error?.data?.error}`);
+    } else if (ErrorCode.RateLimitedError === error?.code) {
+      console.log('slack postMessage RateLimitedError');
+      console.log(error?.data);
+      await showHUD(`Failed to post message due to rate limiting. Will retry.`);
+    } else {
+      console.log('slack postMessage error');
+      console.log(error);
+      await showHUD(`Failed to post message. Error code: ${error?.code}. Error: ${error?.data?.error}`);
+    }
+  }
 }
